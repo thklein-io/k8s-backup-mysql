12bf05f0dde0396cbb194c9b269b9e246c6d9e8d
Thorsten Klein <tk@thklein.io>
2024-04-08 07:47:23 +0200

    fix: script formatting

--- resources/notify.sh
+++ resources/notify.sh
@@ -1,3 +1,3 @@
-#/bin/sh
+#!/bin/bash
 
-curl -X POST -d "message=$1" $NOTIFICATION_WEBHOOK_URL
+curl -X POST -d "message=$1" "$NOTIFICATION_WEBHOOK_URL"
--- resources/perform-backup.sh
+++ resources/perform-backup.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/bash
 
 
 # Set the has_failed variable to false. This will change if any of the subsequent database backups/uploads fail.
@@ -11,13 +11,13 @@ for CURRENT_DATABASE in ${TARGET_DATABASE_NAMES//,/ }
 do
 
     # Perform the database backup. Put the output to a variable. If successful upload the backup to S3, if unsuccessful print an entry to the console and the log, and set has_failed to true.
-    if sqloutput=$(mysqldump -u $TARGET_DATABASE_USER -h $TARGET_DATABASE_HOST -p$TARGET_DATABASE_PASSWORD -P $TARGET_DATABASE_PORT $CURRENT_DATABASE 2>&1 > /tmp/$CURRENT_DATABASE.sql)
+    if sqloutput=$(mysqldump -u "$TARGET_DATABASE_USER" -h "$TARGET_DATABASE_HOST" -p"$TARGET_DATABASE_PASSWORD" -P "$TARGET_DATABASE_PORT" "$CURRENT_DATABASE" 2>&1 > /tmp/"$CURRENT_DATABASE".sql)
     then
 
         echo -e "Database backup successfully completed for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
-        backup_name=$CURRENT_DATABASE_$(date +"%Y-%m-%d_%H-%M-%S").sql
+        backup_name="${CURRENT_DATABASE}_$(date +'%Y-%m-%d_%H-%M-%S').sql"
         # Perform the upload to S3. Put the output to a variable. If successful, print an entry to the console and the log. If unsuccessful, set has_failed to true and print an entry to the console and the log
-        if awsoutput=$(aws s3 cp /tmp/$CURRENT_DATABASE.sql $AWS_BUCKET_URI$AWS_BUCKET_BACKUP_PATH/$backup_name 2>&1)
+        if awsoutput=$(aws s3 cp /tmp/"$CURRENT_DATABASE".sql "$AWS_BUCKET_URI""$AWS_BUCKET_BACKUP_PATH"/"$backup_name" 2>&1)
         then
             echo -e "Database backup successfully uploaded for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
         else
@@ -42,7 +42,7 @@ then
     if [ "$NOTIFY_ENABLED" = true ]
     then
         # Put the contents of the database backup logs into a variable
-        logcontents=`cat /tmp/kubernetes-s3-mysql-backup.log`
+        logcontents=$(cat /tmp/kubernetes-s3-mysql-backup.log)
 
         # Send Slack alert
         /notify.sh "One or more backups on database host $TARGET_DATABASE_HOST failed. The error details are included below:" "$logcontents"
