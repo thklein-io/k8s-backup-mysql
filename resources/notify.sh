87a637a40445a1bb8bdeaca4abc896b3abc39e53
Thorsten Klein <tk@thklein.io>
2024-04-08 17:17:49 +0200

    change: compress backup before uploading

--- resources/notify.sh
+++ resources/notify.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-curl -X POST -d "message=$1" "$NOTIFICATION_WEBHOOK_URL"
+provider="$NOTIFICATION_PROVIDER"
+
+# Send notification to webhook
+if [ "$provider" == "webhook" ]; then
+    curl -X POST -d "message=$1" "$NOTIFICATION_WEBHOOK_URL"
+# Send notification via Telegram
+elif [ "$provider" == "telegram" ]; then
+    curl -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" -d chat_id="$TELEGRAM_CHAT_ID" -d text="$1"
+fi
--- resources/perform-backup.sh
+++ resources/perform-backup.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-
 # Set the has_failed variable to false. This will change if any of the subsequent database backups/uploads fail.
 has_failed=false
 backup_name=""
 
-
 # Loop through all the defined databases, seperating by a ,
 for CURRENT_DATABASE in ${TARGET_DATABASE_NAMES//,/ }
 do
+    # Define backup file name with date
+    backup_name="${CURRENT_DATABASE}_$(date +'%Y-%m-%d_%H-%M-%S').sql"
+    backup_tar_name="${CURRENT_DATABASE}_$(date +'%Y-%m-%d_%H-%M-%S').tar.gz"
 
-    # Perform the database backup. Put the output to a variable. If successful upload the backup to S3, if unsuccessful print an entry to the console and the log, and set has_failed to true.
-    if sqloutput=$(mysqldump -u "$TARGET_DATABASE_USER" -h "$TARGET_DATABASE_HOST" -p"$TARGET_DATABASE_PASSWORD" -P "$TARGET_DATABASE_PORT" "$CURRENT_DATABASE" 2>&1 > /tmp/"$CURRENT_DATABASE".sql)
+    # Perform the database backup. If successful, compress and upload the backup to S3, if unsuccessful print an entry to the console and the log, and set has_failed to true.
+    if sqloutput=$(mysqldump -u "$TARGET_DATABASE_USER" -h "$TARGET_DATABASE_HOST" -p"$TARGET_DATABASE_PASSWORD" -P "$TARGET_DATABASE_PORT" "$CURRENT_DATABASE" > /tmp/"$backup_name" 2>&1)
     then
+        # Compress the SQL dump file
+        tar -czf /tmp/"$backup_tar_name" -C /tmp "$backup_name"
 
-        echo -e "Database backup successfully completed for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
-        backup_name="${CURRENT_DATABASE}_$(date +'%Y-%m-%d_%H-%M-%S').sql"
-        # Perform the upload to S3. Put the output to a variable. If successful, print an entry to the console and the log. If unsuccessful, set has_failed to true and print an entry to the console and the log
-        if awsoutput=$(aws s3 cp /tmp/"$CURRENT_DATABASE".sql "$AWS_BUCKET_URI""$AWS_BUCKET_BACKUP_PATH"/"$backup_name" 2>&1)
+        echo -e "Database backup and compression successfully completed for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
+
+        # Perform the upload to S3. If successful, print an entry to the console and the log. If unsuccessful, set has_failed to true and print an entry to the console and the log.
+        if awsoutput=$(aws s3 cp /tmp/"$backup_tar_name" "$AWS_BUCKET_URI""$AWS_BUCKET_BACKUP_PATH"/"$backup_tar_name" 2>&1)
         then
             echo -e "Database backup successfully uploaded for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S')."
         else
@@ -25,19 +28,17 @@ do
             has_failed=true
         fi
 
+        # Cleanup: Remove the temporary files after uploading
+        rm -f /tmp/"$backup_name" /tmp/"$backup_tar_name"
     else
         echo -e "Database backup FAILED for $CURRENT_DATABASE at $(date +'%d-%m-%Y %H:%M:%S'). Error: $sqloutput" | tee -a /tmp/kubernetes-s3-mysql-backup.log
         has_failed=true
     fi
-
 done
-
-
 
 # Check if any of the backups have failed. If so, exit with a status of 1. Otherwise exit cleanly with a status of 0.
 if [ "$has_failed" = true ]
 then
-
     # If Slack alerts are enabled, send a notification alongside a log of what failed
     if [ "$NOTIFY_ENABLED" = true ]
     then
@@ -50,15 +51,12 @@ then
 
     echo -e "kubernetes-s3-mysql-backup encountered 1 or more errors. Exiting with status code 1."
     exit 1
-
 else
-
     # If Slack alerts are enabled, send a notification that all database backups were successful
     if [ "$NOTIFY_ENABLED" = true ]
     then
-        /notify.sh "Backup created: $backup_name"
+        /notify.sh "Backup created: $backup_tar_name"
     fi
 
     exit 0
-
 fi
